15212d0ecef2a1bc2b06cace43a8264dabc0cfd2
Chun Jiang <cjiang@wework.com>
2018-02-13 23:00:15 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,3 +5,5 @@ display_rainbow = ['R: red','O: orange','Y: yellow','G: green','B: blue','I: ind
 #puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, Y: #{display_rainbow[2]}, G: #{display_rainbow[3]}, B: #{display_rainbow[4]}, I: #{display_rainbow[5]}, V: #{display_rainbow[6]}"
 
 def color(display_rainbow)
+  puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, Y: #{display_rainbow[2]}, G: #{display_rainbow[3]}, B: #{display_rainbow[4]}, I: #{display_rainbow[5]}, V: #{display_rainbow[6]}"
+end
